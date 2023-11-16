056cbe4d55cb711fdbd30bd2741ee080b2195b77
Júlio <julio@jcsr.com.br>
2023-11-16 15:50:27 -0300

    up: `getClients`

--- controllers/getClients.js
+++ controllers/getClients.js
@@ -30,19 +30,19 @@ async function getClients(token, req, res) {
 
         for (let item of clientes) {
             console.log(`Esse é o CNPJ => ${item.cpfCnpj}`)
-            console.log(`Esse é o ID => ${item.id}`)
+            
 
             let idAuvocliente = item.id
             let cnpj = item.cpfCnpj
             let cnpjOriginal = item.cpfCnpj.toString().replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/, "$1.$2.$3/$4-$5");
-            console.log(cnpjOriginal)
+            
             let verClient = await clients.findAll({
                 where: {
                     idAuvo: idAuvocliente
                 }
             })
         
-            if (verClient.length === 0) {
+            if (verClient.length < 1) {
 
                 //busca pelo cnpj
                 let verCnpj = await clients.findAll({
